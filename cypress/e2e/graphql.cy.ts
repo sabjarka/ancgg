6af139cb048e227d447db4b4df89e9b8ba4d9a61
sabjarka <sabjarka@gmail.com>
2023-06-28 10:14:14 +0200

    add one more graphql test

--- cypress/e2e/graphql.cy.ts
+++ cypress/e2e/graphql.cy.ts
@@ -17,4 +17,22 @@ describe('Graphql mocking tests', () => {
     });
     cy.get('.mat-table > tbody > tr').should('have.length', 0)
   });
+
+
+  it('Check response match from graphql into the table', () => {
+    Cypress.on('uncaught:exception', () => {
+      return false;
+    });
+    cy.intercept('GET', 'https://api-stage.csgoroll.com/graphql?operationName=DiceBets*', (req) => {
+    }).as('DiceBets')
+    cy.visit('https://csgoroll-www-master-h7r4kpopga-uc.a.run.app/dice', {
+      auth: {
+        username: 'ancient',
+        password: 'things',
+      }
+    });
+    cy.wait('@DiceBets').then((interception) => {
+      cy.contains(interception.response?.body.data.diceBets.edges[0].node.chance)
+    })
+  });
 });
